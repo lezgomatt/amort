b89e1f772bdd4eb0ebd63d766af6874be781ac8a
Matthew Go <lezgomatt@gmail.com>
2025-01-29 22:45:19 +0800

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+  import {
+    ButtonGroup, Input, InputAddon, Label,
+    Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell,
+  } from "flowbite-svelte";
+  import "./app.css";
+
+  import { calculateSchedule } from "./calculator.ts";
+
+  let inputPrincipalAmount: string = $state("100000.00");
+  let inputLoanTerm: string = $state("6");
+  let inputMonthlyAddOn: string = $state("1.00");
+  let { effectiveRate, schedule } = $derived(calculateSchedule(
+    Number(inputPrincipalAmount),
+    Number(inputLoanTerm),
+    Number(inputMonthlyAddOn) / 100,
+  ));
+</script>
+
+<main class="m-12 p-6 border rounded max-w-7xl bg-gray-100">
+  <div class="flex flex-col gap-8">
+    <div class="flex flex-col gap-4">
+      <div class="max-w-xs">
+        <Label for="principalAmount" class="mb-1 text-md font-semibold">Principal amount</Label>
+        <ButtonGroup class="w-full">
+          <InputAddon>PHP</InputAddon>
+          <Input type="text" id="principalAmount" class="text-right" required bind:value={inputPrincipalAmount} />
+        </ButtonGroup>
+      </div>
+
+      <div class="max-w-xs">
+        <Label for="loanTerm" class="mb-1 text-md font-semibold">Loan term</Label>
+        <ButtonGroup class="w-full">
+          <Input type="text" id="loanTerm" class="text-right" required bind:value={inputLoanTerm} />
+          <InputAddon>month(s)</InputAddon>
+        </ButtonGroup>
+      </div>
+
+      <div class="max-w-xs">
+        <Label for="monthlyAddOn" class="mb-1 text-md font-semibold">Monthly add-on</Label>
+        <ButtonGroup class="w-full">
+          <Input type="text" id="monthlyAddOn" class="text-right" required bind:value={inputMonthlyAddOn} />
+          <InputAddon>%</InputAddon>
+        </ButtonGroup>
+      </div>
+
+      <div class="max-w-xs">
+        <Label for="effectiveRate" class="mb-1 text-md font-semibold">Effective rate</Label>
+        <ButtonGroup class="w-full">
+          <Input type="text" id="effectiveRate" class="text-right" readonly value={effectiveRate * 100} />
+          <InputAddon>%</InputAddon>
+        </ButtonGroup>
+      </div>
+    </div>
+
+    <Table>
+      <TableHead>
+        <TableHeadCell class="text-right">Month</TableHeadCell>
+        <TableHeadCell class="text-right">Starting balance</TableHeadCell>
+        <TableHeadCell class="text-right">Payment</TableHeadCell>
+        <TableHeadCell class="text-right">Principal</TableHeadCell>
+        <TableHeadCell class="text-right">Interest</TableHeadCell>
+        <TableHeadCell class="text-right">Ending balance</TableHeadCell>
+      </TableHead>
+      <TableBody tableBodyClass="divide-y">
+        {#each schedule as s}
+          <TableBodyRow>
+            <TableBodyCell class="text-right">{s.month}</TableBodyCell>
+            <TableBodyCell class="text-right">PHP {s.startingBalance}</TableBodyCell>
+            <TableBodyCell class="text-right">PHP {s.totalPayment}</TableBodyCell>
+            <TableBodyCell class="text-right">PHP {s.principalPayment}</TableBodyCell>
+            <TableBodyCell class="text-right">PHP {s.interestPayment}</TableBodyCell>
+            <TableBodyCell class="text-right">PHP {s.endingBalance}</TableBodyCell>
+          </TableBodyRow>
+        {/each}
+      </TableBody>
+    </Table>
+  </div>
+</main>
---
+++ src/calculator.ts
@@ -0,0 +1,83 @@
+const NUM_DECIMAL_PLACES = 2;
+
+export function calculateSchedule(principal: number, numPeriods: number, addOnRate: number) {
+    let totalInterest = round(principal * addOnRate * numPeriods);
+    let totalPayment = round(principal + totalInterest);
+
+    let paymentPerPeriod = round((principal / numPeriods) + (principal * addOnRate));
+    let finalPayment = round(totalPayment - paymentPerPeriod * (numPeriods - 1));
+
+    let f = (rate: number) => simulate(principal, numPeriods, paymentPerPeriod, finalPayment, totalInterest, rate);
+    let range = [addOnRate / 2, addOnRate * 4] as const;
+
+    let effectiveRate = findRoot(f, range);
+    let schedule = [];
+
+    let balance = principal;
+    let interestAcc = 0;
+
+    for (let i = 1; i <= numPeriods; i++) {
+        let isLastPayment = i === numPeriods;
+        let totalPayment = isLastPayment ? finalPayment : paymentPerPeriod;
+
+        let interestPayment = isLastPayment ? round(totalInterest - interestAcc) : round(balance * effectiveRate);
+        interestAcc += interestPayment;
+        let principalPayment = totalPayment - interestPayment;
+
+        schedule.push({
+            month: i,
+            startingBalance: balance.toFixed(NUM_DECIMAL_PLACES),
+            totalPayment: totalPayment.toFixed(NUM_DECIMAL_PLACES),
+            principalPayment: principalPayment.toFixed(NUM_DECIMAL_PLACES),
+            interestPayment: interestPayment.toFixed(NUM_DECIMAL_PLACES),
+            endingBalance: (balance -= principalPayment).toFixed(NUM_DECIMAL_PLACES),
+        });
+    }
+
+    return { effectiveRate, schedule };
+}
+
+function simulate(principal: number, numPeriods: number, paymentPerPeriod: number, finalPayment: number, totalInterest: number, effectiveRate: number): number {
+    let balance = principal;
+
+    for (let i = 1; i <= numPeriods; i++) {
+        let isLastPayment = i === numPeriods;
+        let totalPayment = isLastPayment ? finalPayment : paymentPerPeriod;
+
+        let interestPayment = round(balance * effectiveRate);
+        let principalPayment = totalPayment - interestPayment;
+        balance -= principalPayment;
+    }
+
+    return balance;
+}
+
+// Find the root using the bisection method
+function findRoot(f: (x: number) => number, range: readonly [number, number], maxIterations = 100): number {
+    let [low, high] = range;
+    let guess = NaN;
+
+    let triesLeft = maxIterations;
+    while (true) {
+        if (triesLeft-- <= 0) {
+            return guess;
+        }
+
+        guess = (low + high) / 2;
+        let y = f(guess);
+        // favor positive balance, since we can just adjust the interest down for it
+        if (y > 0 && (Math.abs(y) < 1e-12 || Math.abs(high - low) < 1e-16)) {
+            return guess;
+        } else if (y > 0) {
+            high = guess;
+        } else {
+            low = guess;
+        }
+    }
+}
+
+function round(value: number): number {
+    let factor = Math.pow(10, NUM_DECIMAL_PLACES);
+
+    return Math.round(value * factor) / factor;
+}
