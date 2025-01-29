92986ccfdff07ef93416a1abc1467396b129d1df
Matthew Go <lezgomatt@gmail.com>
2025-01-29 22:54:35 +0800

    bugfix: Fix assets path

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./", // Make it relative so it'll load on GH Pages
   build: {
-    outDir: "docs", // to easily publish on GH pages
+    outDir: "docs", // to easily publish on GH Pages
   },
   plugins: [svelte()],
 });
