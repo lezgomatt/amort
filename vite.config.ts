a731ce21ce0f75fc474a474d195adfacd873d0f0
Matthew Go <lezgomatt@gmail.com>
2025-01-29 22:47:48 +0800

    Publish a build for GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "docs", // to easily publish on GH pages
+  },
   plugins: [svelte()],
 });
